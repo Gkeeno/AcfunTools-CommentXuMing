08cebd4a88fc0a909220e43bfcd9a5c7905085d9
Gkeeno <zxmui@outlook.com>
2020-01-22 19:48:30 +0800

    added 查找评论页面的功能实现

--- AcfunTools.CommentXuMing.Web/Controllers/HomeController.cs
+++ AcfunTools.CommentXuMing.Web/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AcfunTools.CommentXuMing.Web.Models;
+using AcfunTools.CommentXuMing.Model;
 
 namespace AcfunTools.CommentXuMing.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CommentXuMingDbContext _dbcontext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CommentXuMingDbContext dbcontext)
         {
             _logger = logger;
+            _dbcontext = dbcontext;
         }
 
         public IActionResult Index()
@@ -26,6 +29,24 @@ namespace AcfunTools.CommentXuMing.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(SreachViewModel sreach)
+        {
+            if (!ModelState.IsValid) return Ok(new { });
+
+            var result = _dbcontext.Comments.FirstOrDefault(c => c.AcNo == sreach.AcNo && c.Floor == sreach.Floor);
+            if (result == null)
+            {
+                return Ok(new { });
+            }
+
+            return Ok(new
+            {
+                msg = "",
+                data = result
+            });
         }
 
         public IActionResult Admin()
---
+++ AcfunTools.CommentXuMing.Web/Models/SreachViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcfunTools.CommentXuMing.Web.Models
+{
+    public class SreachViewModel
+    {
+        public string AcNo { get; set; }
+        public int Floor { get; set; }
+    }
+}
