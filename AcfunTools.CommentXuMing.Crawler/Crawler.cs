77e28b18fbca1d74e65aaf4588492f35c8bde011
Gkeeno <zxmui@outlook.com>
2020-01-25 01:59:58 +0800

    changed 优化爬取逻辑->取最后三页评论处理；时间间隔修改

--- AcfunTools.CommentXuMing.Crawler/Crawler.cs
+++ AcfunTools.CommentXuMing.Crawler/Crawler.cs
@@ -54,9 +54,9 @@ namespace AcfunTools.CommentXuMing.Crawler
                     {
                         Console.WriteLine($"[Crawler]{DateTime.Now} ProcessStart..............");
                         await RunProcess();
-                        await Task.Delay(800);
-                        await RunProcess("情感区");
                         await Task.Delay(CrawlerConstant.IntervalMillisecond_RefreshArticle);
+                        await RunProcess("情感区");
+                        await Task.Delay(CrawlerConstant.IntervalMillisecond_RefreshArticle - 400);
                         Console.WriteLine($"[Crawler]{DateTime.Now} ProcessOver..............处理文章数:{_commentFetchContexts.Count}");
                     }
                 }
